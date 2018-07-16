6064fb0429a3e18aca9be859dce0f29dd701c19c
Jason Forbes <jforbes@krakensonar.com>
2018-07-16 16:00:46 -0230

    Added tmux support and helper functions

---
+++ .bash_helpers.sh
@@ -0,0 +1,21 @@
+log(){
+  if [ -z ${1} ]; then
+    dir=$(pwd);
+  else
+    dir=${1};
+  fi
+  cd ${dir}
+  tmux new -s gtd \; \
+    split-window -h \; \
+    split-window -v \; \
+    select-pane -t 0 \; \
+    send-keys 'vi NOTES.txt' C-m \; \
+    select-pane -t 1 \; \
+    send-keys 'vi TODO.txt' C-m\; \
+    select-pane -t 2 \; \
+    send-keys 'vi SOMEDAY.txt' C-m\;
+}
+
+if [[ ${TERM_PROGRAM} =~ Hyper.* ]]; then
+    exec zsh
+fi
